b5138dfddaadc9cf90fc772798752986864966ea
Preethi1602 <107020233+Preethi1602@users.noreply.github.com>
2023-07-13 21:25:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,8 +15,6 @@ if __name__ == '__main__':
     
     api_url = "https://jsonplaceholder.typicode.com/posts"
     response_time = make_api_call(api_url)
-
-    # print("Response Status Code:", response)
     print("Response Time:", response_time, "seconds")
     with Configurator() as config:
         app = config.make_wsgi_app()
