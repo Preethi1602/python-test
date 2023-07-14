bdad8e5db0bafb0840e0c98fdf543c2687f36002
Preethi1602 <107020233+Preethi1602@users.noreply.github.com>
2023-07-14 09:26:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
 import time
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
 import os
 
 def make_api_call(url):
@@ -10,13 +7,7 @@ def make_api_call(url):
     response_time = end_time - start_time
     return  response_time
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    
+if __name__ == '__main__':    
     api_url = "https://jsonplaceholder.typicode.com/posts"
     response_time = make_api_call(api_url)
     print("Response Time:", response_time, "seconds")
-    with Configurator() as config:
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
