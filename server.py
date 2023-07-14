e5b1ae4cb26503dc419f99f1562de5f7d4aba65e
Preethi1602 <107020233+Preethi1602@users.noreply.github.com>
2023-07-14 09:28:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
+import requests
 import time
-import os
+from wsgiref.simple_server import make_server
 
 def make_api_call(url):
     start_time = time.time()
+    response = requests.get(url)
     end_time = time.time()
     response_time = end_time - start_time
-    return  response_time
+    return response, response_time
 
 if __name__ == '__main__':    
     api_url = "https://jsonplaceholder.typicode.com/posts"
-    response_time = make_api_call(api_url)
+    response, response_time = make_api_call(api_url)
+
     print("Response Time:", response_time, "seconds")
